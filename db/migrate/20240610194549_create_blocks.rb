dc530443e87156a1fc9e4da173d0dc5b9a1509a2
Bret <breton.burnett@gmail.com>
2024-06-10 15:08:14 -0500

    Create and run  migration to block users

---
+++ db/migrate/20240610194549_create_blocks.rb
@@ -0,0 +1,11 @@
+class CreateBlocks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :blocks, id: :uuid, default: -> { "gen_random_uuid()" } do |t|
+      t.references :user, null: false, foreign_key: true, type: :uuid
+      t.references :blocked_user, null: false, foreign_key: { to_table: :users }, type: :uuid
+      t.boolean :blocked, default: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,59 +10,71 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_605_234_719) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_10_194549) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'pgcrypto'
-  enable_extension 'plpgsql'
+  enable_extension "pgcrypto"
+  enable_extension "plpgsql"
 
-  create_table 'follows', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
-    t.uuid 'following_user_id'
-    t.uuid 'followed_user_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['followed_user_id'], name: 'index_follows_on_followed_user_id'
-    t.index ['following_user_id'], name: 'index_follows_on_following_user_id'
+  create_table "blocks", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.uuid "blocked_user_id", null: false
+    t.boolean "blocked", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blocked_user_id"], name: "index_blocks_on_blocked_user_id"
+    t.index ["user_id"], name: "index_blocks_on_user_id"
   end
 
-  create_table 'posts', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
-    t.string 'description', default: ''
-    t.uuid 'user_id', null: false
-    t.boolean 'close_friends', default: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_posts_on_user_id'
+  create_table "follows", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "following_user_id"
+    t.uuid "followed_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_user_id"], name: "index_follows_on_followed_user_id"
+    t.index ["following_user_id"], name: "index_follows_on_following_user_id"
   end
 
-  create_table 'users', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.integer 'sign_in_count', default: 0, null: false
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.string 'current_sign_in_ip'
-    t.string 'last_sign_in_ip'
-    t.integer 'failed_attempts', default: 0, null: false
-    t.string 'unlock_token'
-    t.datetime 'locked_at'
-    t.string 'username', default: ''
-    t.boolean 'show_email', default: false
-    t.string 'bio', default: ''
-    t.boolean 'active', default: true
-    t.string 'role', default: 'STANDARD'
-    t.string 'avatar_url', default: ''
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'jti'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['jti'], name: 'index_users_on_jti'
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
-    t.index ['username'], name: 'index_users_on_username', unique: true
+  create_table "posts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "description", default: ""
+    t.uuid "user_id", null: false
+    t.boolean "close_friends", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  add_foreign_key 'follows', 'users', column: 'followed_user_id'
-  add_foreign_key 'follows', 'users', column: 'following_user_id'
-  add_foreign_key 'posts', 'users'
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.string "username", default: ""
+    t.boolean "show_email", default: false
+    t.string "bio", default: ""
+    t.boolean "active", default: true
+    t.string "role", default: "STANDARD"
+    t.string "avatar_url", default: ""
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "jti"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
+  add_foreign_key "blocks", "users"
+  add_foreign_key "blocks", "users", column: "blocked_user_id"
+  add_foreign_key "follows", "users", column: "followed_user_id"
+  add_foreign_key "follows", "users", column: "following_user_id"
+  add_foreign_key "posts", "users"
 end
