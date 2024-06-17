9b434d9fd168da858debbf46610b47f56c677fd2
Bret <breton.burnett@gmail.com>
2024-06-17 17:22:26 -0500

    small updates

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,14 +1,15 @@
 class Event < ApplicationRecord
-  validates :description, :start_time, :end_time, :title, presence: true
-  validates :description, length: { minimum: 20 }
-	validates :title, length: { minimum: 5 }
-  validate :start_time_before_end_time, :start_time_and_end_time_are_datetime
-  
-	has_many :event_guests, dependent: :destroy
+  has_many :event_guests, dependent: :destroy
   has_many :users, through: :event_guests
 
+  validates :description, :start_time, :end_time, :title, presence: true
+  validates :description, length: { minimum: 20 }
+  validates :title, length: { minimum: 5 }
+  validate :start_time_before_end_time
+  validate :start_time_and_end_time_are_datetime
+
 	def deactivate_if_past
-    self.update(active: false) if self.end_time < Time.current
+    self.update(active: false) if self.end_time < Time.zone.now
   end
 
   private
@@ -20,8 +21,8 @@ class Event < ApplicationRecord
   end
 
   def start_time_and_end_time_are_datetime
-    errors.add(:start_time, "must be a datetime") unless start_time.is_a?(ActiveSupport::TimeWithZone)
-    errors.add(:end_time, "must be a datetime") unless end_time.is_a?(ActiveSupport::TimeWithZone)
+    [:start_time, :end_time].each do |time|
+      errors.add(time, "must be a datetime") unless send(time).is_a?(ActiveSupport::TimeWithZone)
+    end
   end
 end
- 
